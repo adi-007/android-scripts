3954697f2c72c3b49feaae5e94fa4873fee5946b
adi-007 <chaudharyaaditya007@gmail.com>
2018-06-06 12:16:20 +0530

    comments added

--- tinder/rightSwipe.sh
+++ tinder/rightSwipe.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 
-arg1=$1  #input from the first argument
+# IMPORTANT:
+# 1) Change the coordinates of the GPS ON button(that tinder pops up when you open the app) 
+#    (here it is set to 880 1153) and right swipe button(here it is set to 733 1769) according
+#	 to your phone. 
+# 2) Add the <device ID> (example: "F6AZFG144596") as obtained from the output of "./adb devices"
+# 	 command.  
+
+
+# Input from the first argument. It signifies the no. of right swipes that the user wants
+arg1=$1  
+
 counter=1
 
 cd /home/adi/Android_SDK/platform-tools
 ./adb devices
-./adb -s F6AZFG177486 shell "
+./adb -s <device ID> shell "
+
 am start -n com.tinder/.activities.LoginActivity
 
 sleep 3
 input tap 880 1153
 input tap 880 1153
 input tap 880 1153
-sleep 2
 exit
 "
 
 while [ $counter -le $arg1 ]
 do
-  ./adb -s F6AZFG177486 shell "
-  input tap 733 1769
-  input tap 733 1769
-  input tap 733 1769
+  ./adb -s <device ID> shell "
   input tap 733 1769
   exit
   "
