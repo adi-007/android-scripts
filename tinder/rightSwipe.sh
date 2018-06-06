bc4752c3600e45778e4e5584b441c9720d340251
Aaditya Chaudhary <chaudharyaaditya007@gmail.com>
2018-06-06 12:22:30 +0530

    updated text reflow

--- tinder/rightSwipe.sh
+++ tinder/rightSwipe.sh
@@ -3,7 +3,7 @@
 # IMPORTANT:
 # 1) Change the coordinates of the GPS ON button(that tinder pops up when you open the app) 
 #    (here it is set to 880 1153) and right swipe button(here it is set to 733 1769) according
-#	 to your phone. 
+#	   to your phone. 
 # 2) Add the <device ID> (example: "F6AZFG144596") as obtained from the output of "./adb devices"
 # 	 command.  
 
